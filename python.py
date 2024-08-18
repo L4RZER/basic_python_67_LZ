f1c80c07ce8de78d5f9c644ebcf02c29bcb406fc
Nattapong <natnat50926@gmail.com>
2024-08-18 11:40:15 +0700

    Python Colletions

--- python.py
+++ python.py
@@ -10,3 +10,33 @@ myvariableName = "John"
 Myvariablename = "John"
 # Snake Case
 my_variable_name = "John"
+
+"""
+#
+# Part python string
+#
+"""
+x = "Hey Bro"
+print(x)
+
+y = """1 Hey Bro
+2 Hey Bro
+3 Hey Bro
+"""
+print(y)
+
+x = "Hey Bro"
+print(x[2])
+print(len(x))
+print("Hey" in x)
+print("What 'up" not in x)
+print(x.upper())
+print(x.lower())
+
+
+print(x.replace("Bro", "Sis"))
+print(x.split(" "))
+
+a = "Apple"
+b = "Company"
+print(a + " " + b)
